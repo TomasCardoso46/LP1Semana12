5df943c82cbca0b5f10610ca0abbdbdcb27f7f09
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-30 22:02:03 +0100

    Added IsStringPalindrome as a local func

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -13,6 +13,39 @@ namespace Palindrome
             }
         }
 
+        static bool IsPalindrome(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input), "String cannot be null");
+            }
+
+            if (input.Length < 2)
+            {
+                return true;
+            }
+
+            bool IsStringPalindrome(string str)
+            {
+                int left = 0;
+                int right = str.Length - 1;
+
+                while (left < right)
+                {
+                    if (str[left] != str[right])
+                    {
+                        return false;
+                    }
+
+                    left++;
+                    right--;
+                }
+
+                return true;
+            }
+
+            return IsStringPalindrome(input);
+        }
     }
 }
 
